4e84d7a39ece7db259bbc062a0312bf895161e83
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-07-09 18:16:51 +0200

    Remove max resolution limit in image loader

--- RemoveZoomLimit/src/main/java/com/aliucord/plugins/RemoveZoomLimit.java
+++ RemoveZoomLimit/src/main/java/com/aliucord/plugins/RemoveZoomLimit.java
@@ -11,6 +11,8 @@ import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
+import com.airbnb.lottie.parser.AnimatableValueParser;
+import com.aliucord.Utils;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.PinePatchFn;
 
@@ -24,7 +26,7 @@ public class RemoveZoomLimit extends Plugin {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[]{ new Manifest.Author("Juby210", 324622488644616195L) };
         manifest.description = "Removes maximum zoom limit.";
-        manifest.version = "1.0.3";
+        manifest.version = "1.0.4";
         manifest.updateUrl = "https://raw.githubusercontent.com/Juby210/Aliucord-plugins/builds/updater.json";
         return manifest;
     }
@@ -34,7 +36,7 @@ public class RemoveZoomLimit extends Plugin {
         // load full resolution to see details while zooming
         patcher.patch("com.discord.widgets.media.WidgetMedia", "getFormattedUrl", new Class<?>[]{ Context.class, Uri.class }, new PinePatchFn(callFrame -> {
             var res = (String) callFrame.getResult();
-            if (res.startsWith("https://media.discordapp.net")) {
+            if (res.contains(".discordapp.net/")) {
                 var arr = res.split("\\?");
                 callFrame.setResult(arr[0] + (arr[1].contains("format=") ? "?format=" + arr[1].split("format=")[1] : ""));
             }
@@ -43,6 +45,16 @@ public class RemoveZoomLimit extends Plugin {
         // com.facebook.samples.zoomable.DefaultZoomableController limitScale
         // https://github.com/facebook/fresco/blob/master/samples/zoomable/src/main/java/com/facebook/samples/zoomable/DefaultZoomableController.java#L474-L495
         patcher.patch("c.f.l.b.c", "f", new Class<?>[]{ Matrix.class, float.class, float.class, int.class }, MethodReplacement.returnConstant(false));
+
+        // Remove max resolution limit in image loader
+        // Gets method by param types because the method is in heavily obfuscated class
+        for (var m : AnimatableValueParser.class.getDeclaredMethods()) {
+            var params = m.getParameterTypes();
+            if (params.length == 4 && params[0] == c.f.j.d.f.class && params[1] == c.f.j.d.e.class && params[3] == int.class) {
+                Utils.log("[RemoveZoomLimit] Found obfuscated method to limit resolution: " + m.getName());
+                patcher.patch(m, MethodReplacement.returnConstant(1));
+            }
+        }
     }
 
     @Override
