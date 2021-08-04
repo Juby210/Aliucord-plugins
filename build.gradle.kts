b91f18d213cee564a2ab4bfa22b1ef79cf09a350
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-08-04 14:47:09 +0200

    update build script

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    if (name != "Aliucord" && name != "DiscordStubs") {
+        apply(plugin = "com.android.library")
+
+        android {
+            compileSdkVersion(30)
+
+            defaultConfig {
+                minSdk = 24
+                targetSdk = 30
+                versionCode = 1
+                versionName = "1.0"
+            }
+
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+
+        dependencies {
+            val implementation by configurations
+
+            implementation(project(":Aliucord"))
+
+            implementation("androidx.appcompat:appcompat:1.3.1")
+            implementation("com.google.android.material:material:1.4.0")
+            implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+        }
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
