beafa62a6529fc22d279ce53495d4b440778936b
Vendicated <vendicated@riseup.net>
2021-11-13 17:03:58 +0100

    Add copy buttons

--- ViewRaw/build.gradle.kts
+++ ViewRaw/build.gradle.kts
@@ -1,9 +1,9 @@
-version = "1.0.7"
+version = "1.0.8"
 description = "View & Copy raw message and markdown."
 
 aliucord.changelog.set("""
     Updated {updated marginTop}
     ======================
     
-    * updated to **101.3**
+    * added copy buttons in both the ChatListActions list and the view raw tab
 """.trimIndent())
