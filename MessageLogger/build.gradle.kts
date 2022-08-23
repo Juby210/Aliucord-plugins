18a215a3adf1c2c8f9543bee477b0561c070b15c
00-XPRT-00 <74925728+00-XPRT-00@users.noreply.github.com>
2022-08-24 00:54:08 +0100

    Add Changelog

--- MessageLogger/build.gradle.kts
+++ MessageLogger/build.gradle.kts
@@ -1,2 +1,11 @@
 version = "1.0.0"
 description = "Saves deleted / edited messages to a SQLite database and restores them when the channel is opened."
+
+aliucord.changelog.set("""
+    1.0.0
+    ======================
+
+    * Added SQLite saving for deleted messages and edit history
+    * Added the ability to clear data in plugin settings
+
+""".trimIndent())
