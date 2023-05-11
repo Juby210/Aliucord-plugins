1fe1cb54c916156ae38591f9127d48e51300ffd2
Juby210 <31005896+Juby210@users.noreply.github.com>
2023-05-11 15:35:00 +0200

    NewEmojis v1.0.1

--- NewEmojis/build.gradle.kts
+++ NewEmojis/build.gradle.kts
@@ -1,2 +1,10 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Adds new emojis missing in old discord."
+
+aliucord.changelog.set("""
+    1.0.1 {added marginTop}
+    ======================
+
+    * updated twemoji url to 14.1.2 (jdecked's fork)
+    * added option to revert change of pleading and face_holding_back_tears emoji
+""".trimIndent())
--- NewEmojis/src/main/java/io/github/juby210/acplugins/NewEmojis.kt
+++ NewEmojis/src/main/java/io/github/juby210/acplugins/NewEmojis.kt
@@ -1,30 +1,57 @@
 /*
- * Copyright (c) 2021-2022 Juby210
+ * Copyright (c) 2021-2023 Juby210
  * Licensed under the Open Software License version 3.0
  */
 
 package io.github.juby210.acplugins
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
+import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.api.SettingsAPI
 import com.aliucord.entities.Plugin
+import com.aliucord.widgets.BottomSheet
 import com.discord.models.domain.Model
 import com.discord.models.domain.emoji.*
 import com.discord.stores.*
+import com.discord.views.CheckedSetting
 import de.robv.android.xposed.XC_MethodHook
 import java.io.InputStreamReader
 
 @AliucordPlugin
 @Suppress("UNCHECKED_CAST")
 class NewEmojis : Plugin() {
+    class Settings(private val settings: SettingsAPI) : BottomSheet() {
+        override fun onViewCreated(view: View, bundle: Bundle?) {
+            super.onViewCreated(view, bundle)
+
+            val context = requireContext()
+            addView(Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Use new pleading", null).apply {
+                val key = "newPleading"
+                isChecked = settings.getBool(key, true)
+                setOnCheckedListener { settings.setBool(key, it) }
+            })
+            addView(Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Use new face_holding_back_tears", null).apply {
+                val key = "newFaceHoldingBackTears"
+                isChecked = settings.getBool(key, true)
+                setOnCheckedListener { settings.setBool(key, it) }
+            })
+        }
+    }
+
     init {
         needsResources = true
+        settingsTab = SettingsTab(Settings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
     }
 
     override fun start(ctx: Context?) {
-        val model = Model.JsonReader(InputStreamReader(
-            resources.openRawResource(resources.getIdentifier("emojis", "raw", "io.github.juby210.acplugins"))
-        )).parse(ModelEmojiUnicode.Bundle())
+        val model = Model.JsonReader(
+            InputStreamReader(
+                resources.openRawResource(resources.getIdentifier("emojis", "raw", "io.github.juby210.acplugins"))
+            )
+        ).parse(ModelEmojiUnicode.Bundle())
 
         val c = StoreEmoji::class.java
         val unicodeEmojisF = c.getDeclaredField("unicodeEmojis").apply { isAccessible = true }
@@ -40,8 +67,10 @@ class NewEmojis : Plugin() {
             unicodeEmojisNamesMap[storeEmoji] as HashMap<*, *>
         )
 
-        val pluginCodePoints = ArrayList<String>()
+        // remove old handshake emote emoji without diversities
+        unicodeEmojis[EmojiCategory.PEOPLE]!!.run { removeAt(indexOfFirst { it.firstName == "handshake" }) }
 
+        val pluginCodePoints = ArrayList<String>()
         model.emojis.forEach { (category, emojis) ->
             unicodeEmojis[category]!!.addAll(emojis)
             emojis.forEach { emoji ->
@@ -60,9 +89,18 @@ class NewEmojis : Plugin() {
             object : XC_MethodHook(51) {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     val code = param.args[0] as String
-                    if (pluginCodePoints.contains(code))
-                        param.result = "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/${code.replace("_", "-")}.png"
+                    if (code == "1f97a") {
+                        if (settings.getBool("newPleading", true)) param.result = getUrl(code, true)
+                    } else if (pluginCodePoints.contains(code)) {
+                        param.result = getUrl(
+                            code.replace("_", "-"),
+                            code != "1f979" || settings.getBool("newFaceHoldingBackTears", true)
+                        )
+                    }
                 }
+
+                fun getUrl(code: String, new: Boolean) =
+                    "https://jdecked.github.io/twemoji/v/${if (new) "14.1.2" else "14.1.1"}/72x72/$code.png"
             }
         )
     }
