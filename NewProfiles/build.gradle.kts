6c0a176d4b8d7fa3d160c9a33dd9f6d6dd556c93
Javkhlanbayar Khongorzul <jbk@duck.com>
2023-05-27 21:21:30 +0200

    NewProfiles v0.0.2

--- NewProfiles/build.gradle.kts
+++ NewProfiles/build.gradle.kts
@@ -1,2 +1,8 @@
-version = "0.0.1"
+version = "0.0.2"
 description = "Allows you to view new user profiles"
+
+aliucord.changelog.set("""
+    # 0.0.2
+    * voice settings, admin actions, edit profiles sections are now transparent too
+    * user details sheet's window inset area is now also colorized
+""".trimIndent())
